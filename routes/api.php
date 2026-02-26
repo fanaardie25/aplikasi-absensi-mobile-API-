2da4c2f613e75df7ee28f0f27f63e578c1371562
fanaardie25 <follgrowthindonesia@gmail.com>
2026-02-26 10:19:22 +0700

    change : change endpoint get current user

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/me', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
