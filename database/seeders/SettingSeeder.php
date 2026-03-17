3a01f4d218b1656340fc739689a23b8fd25eb79f
fanaardie25 <follgrowthindonesia@gmail.com>
2026-03-17 10:37:38 +0700

    feat: menambahkan seeder untuk tabel setting

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        Setting::create([
+            'key' => 'school_latitude',
+            'value' => '-7.390022513649234', 
+        ]);
+
+        Setting::create([
+            'key' => 'school_longitude',
+            'value' => '110.51808635390792',
+        ]);
+
+        Setting::create([
+            'key' => 'attendance_radius',
+            'value' => '100', 
+        ]);
+
+        Setting::create([
+            'key' => 'start_time',
+            'value' => '12:00:00',
+        ]);
+        
+        Setting::create([
+            'key' => 'end_time',
+            'value' => '13:00:00',
+        ]);
+
+        Setting::create([
+            'key' => 'site_name',
+            'value' => 'SAKTI',
+        ]);
+    }
+}
